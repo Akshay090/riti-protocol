01c6361df95d138219169f6f75ef81b86649bdef
Akshay <aks28id@gmail.com>
2023-12-09 01:43:18 +0530

    wip akshay

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,87 +1,73 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
+contract HabitFormationPlatform {
+    // Enum to represent refresh frequency
+    enum RefreshFrequency { Day, Week, Month }
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+    // Struct to represent user information
+    struct UserInfo {
+        string platformUsername;
+    }
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-	// State Variables
-	address public immutable owner;
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
-	mapping(address => uint) public userGreetingCounter;
+    // Struct to represent habit configuration
+    struct Config {
+        uint256 startDate;
+        uint256 endDate;
+        uint256 refreshCount;
+        RefreshFrequency refreshFrequency;
+        uint256 lastUpdatedAt;
+    }
 
-	// Events: a way to emit log statements from smart contract that can be listened to by external parties
-	event GreetingChange(
-		address indexed greetingSetter,
-		string newGreeting,
-		bool premium,
-		uint256 value
-	);
+    // Struct to represent a habit
+    struct Habit {
+        address userId;
+        UserInfo userInfo;
+        Config config;
+    }
 
-	// Constructor: Called once on contract deployment
-	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
-	}
+    // Mapping to store user information
+    mapping(address => UserInfo) public users;
 
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
-		_;
-	}
+    // Mapping to store habit information
+    mapping(address => Habit[]) public habits;
 
-	/**
-	 * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-	 *
-	 * @param _newGreeting (string memory) - new greeting to save on the contract
-	 */
-	function setGreeting(string memory _newGreeting) public payable {
-		// Print data to the hardhat chain console. Remove when deploying to a live network.
-		console.log(
-			"Setting new greeting '%s' from %s",
-			_newGreeting,
-			msg.sender
-		);
+    // Function to create a new user
+    function createUser(string memory _platformUsername) public {
+        users[msg.sender] = UserInfo(_platformUsername);
+    }
 
-		// Change state variables
-		greeting = _newGreeting;
-		totalCounter += 1;
-		userGreetingCounter[msg.sender] += 1;
+    // Function to create a new habit for a user
+    function createHabit(
+        uint256 _startDate,
+        uint256 _endDate,
+        uint256 _refreshCount,
+        RefreshFrequency _refreshFrequency
+    ) public {
+        Config memory config = Config({
+            startDate: _startDate,
+            endDate: _endDate,
+            refreshCount: _refreshCount,
+            refreshFrequency: _refreshFrequency,
+            lastUpdatedAt: block.timestamp
+        });
 
-		// msg.value: built-in global variable that represents the amount of ether sent with the transaction
-		if (msg.value > 0) {
-			premium = true;
-		} else {
-			premium = false;
-		}
+        Habit memory newHabit = Habit({
+            userId: msg.sender,
+            userInfo: users[msg.sender],
+            config: config
+        });
 
-		// emit: keyword used to trigger an event
-		emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, 0);
-	}
+        habits[msg.sender].push(newHabit);
+    }
 
-	/**
-	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
-	 */
-	function withdraw() public isOwner {
-		(bool success, ) = owner.call{ value: address(this).balance }("");
-		require(success, "Failed to send Ether");
-	}
+    // Function to update habit status
+    function updateHabitStatus(address _userId, uint256 _habitIndex) public {
+        // Perform status update logic here
 
-	/**
-	 * Function that allows the contract to receive ETH
-	 */
-	receive() external payable {}
+        // Update lastUpdatedAt timestamp
+        habits[_userId][_habitIndex].config.lastUpdatedAt = block.timestamp;
+    }
+
+    // Add other functions as needed for habit tracking and management
 }
