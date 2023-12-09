3f0cc359b1c665ef293842ea1b3935306f830c3c
Akshay <aks28id@gmail.com>
2023-12-09 21:54:31 +0530

    base: static screen 1

--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     RitiProtocol: {
-      address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+      address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
       abi: [
         {
           inputs: [
@@ -213,7 +213,8 @@ const deployedContracts = {
                               type: "bool",
                             },
                           ],
-                          internalType: "struct RitiProtocol.UserRitiInformation[]",
+                          internalType:
+                            "struct RitiProtocol.UserRitiInformation[]",
                           name: "completionStatus",
                           type: "tuple[]",
                         },
@@ -401,7 +402,8 @@ const deployedContracts = {
                               type: "bool",
                             },
                           ],
-                          internalType: "struct RitiProtocol.UserRitiInformation[]",
+                          internalType:
+                            "struct RitiProtocol.UserRitiInformation[]",
                           name: "completionStatus",
                           type: "tuple[]",
                         },
